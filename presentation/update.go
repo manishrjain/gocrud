ebeaf609ba6cc5893fc3639360f4ce04e99eb630
Manish R Jain <manish@snapknock.com>
2015-09-26 18:47:32 +1000

    Store data and query data

--- presentation/update.go
+++ presentation/update.go
@@ -5,19 +5,42 @@ import (
 	"gopkg.in/manishrjain/gocrud.v1/store"
 )
 
-// Create a new Meetup event.
-func main() {
-	// Get entity, set author for update.
-	u := store.NewUpdate("Meetup", "Events").SetSource("ScottBarr")
+// Create a new Social Post.
+func update() {
+	// Get User entity, set author for update.
+	u := store.NewUpdate("User", "userid").SetSource("userid")
 
-	p := u.AddChild("Event") // Add Event to Meetup entity.
-	p.Set("location", "River City Labs").Set("city", "Brisbane").
-		Set("topic", "First Brisbane Gophers meetup") // Method chaining
+	// New Post child entity for User entity.
+	p := u.AddChild("Post").
+		Set("url", "https://xkcd.com/936/").
+		Set("body", "Password Strength") // Method chaining.
+	p.AddChild("Like").Set("upvote", true)
 
 	// Add attendees to Event entity.
-	p.AddChild("Attendee").Set("name", "Scott").Set("organizer", true)
-	p.AddChild("Attendee").Set("name", "Manish").Set("presenter", true)
+	c := p.AddChild("Comment").Set("text", "Hard for humans, easy for computers.")
+	c.AddChild("Like").Set("upvote", true)
 
 	ctx := req.NewContext(3) // Number of chars in unique ids.
 	_ = u.Execute(ctx)       // Returns error
+}
+
+func query() {
+	// Collect all Post entities under User userid.
+	p := store.NewQuery("userid").Collect("Post")
+	p.Collect("Like") // Collect all Like entities for Post entities.
+
+	// Collect all Comment entities, and Likes on those comments.
+	p.Collect("Comment").Collect("Like")
+
+	// Alternatively, get everything up to certain depth.
+	p = store.NewQuery("userid").UptoDepth(10)
+
+	result, _ := p.Run()
+	result.ToJson() // Convert to JSON.
+
+	// Optionally write to w http.ResponseWriter.
+	result.WriteJsonResponse(w)
+}
+
+func main() {
 }
